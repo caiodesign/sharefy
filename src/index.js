6ec6f2bf78acf2846f3f6f3799a452eb21d28c91
Caio Oliveira <caio.oliveira4@nextel.com.br>
2019-01-21 18:25:28 -0200

    setting architecture;

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
+import { hot } from 'react-hot-loader/babel'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const RenderApp = () => (
+  <Router>
+    <div>
+      Hello World
+    </div>
+  </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+export default hot(RenderApp)
+
 serviceWorker.unregister();
